bc1c66c95e08e7c65bb10b2c198c09ad39a88a39
erfan <erfan.seidipoor@ut.ac.ir>
2018-02-26 22:46:58 +0330

    adding socket.io to app

--- server/server.js
+++ server/server.js
@@ -1,14 +1,23 @@
-const express = require('express')
 const Path = require('path')
+const express = require('express')
+
+
 var publicPath = Path.join(__dirname,'../public')
 
 const app = express();
-
-
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 app.use(express.static(publicPath))
 
-// app.get('/', function(req, res) {
-//     res.sendFile(publicPath+'/index.html');
-// });
-app.listen(3000)
+
+io.on('connection', (client)=>{ 
+    console.log('new client connected ');
+    client.on('disconnect',()=>{ console.log('A client disconnected ');});
+ });
+
+
+ server.listen(3000 , ()=>{
+     console.log('server Running On port 3000');
+ });
+
 
